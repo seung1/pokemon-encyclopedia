d5b90a84d487a2a44b351e63ad42ef163318c6ef
seung1 <97tmddnjs@naver.com>
2023-09-17 07:51:13 +0900

    상세페이지 데이터 가져오기

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,6 +1,51 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
+  const params = useParams();
+  const pokemonId = params.id;
+
+  const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
+
+  const getNextAndPreviousPokemon = async (id) => {
+    const urlPokemon = `${baseUrl}?limit=1&offset=${id - 1}`;
+    const { data: pokemonData } = await axios.get(urlPokemon);
+
+    const nextResponse =
+      pokemonData.next && (await axios.get(pokemonData.next));
+    const previousResponse =
+      pokemonData.previous && (await axios.get(pokemonData.previous));
+
+    return {
+      next: nextResponse?.data?.results?.[0].name,
+      previous: previousResponse?.data?.results?.[0].name,
+    };
+  };
+
+  const fetchPokemonData = async () => {
+    const url = `${baseUrl}${pokemonId}`;
+    try {
+      const { data: pokemonData } = await axios.get(url);
+
+      if (pokemonData) {
+        const { name, id, types, weight, height, stats, abilities } =
+          pokemonData;
+
+        const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
+
+        // 진행중
+        console.log(nextAndPreviousPokemon);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    fetchPokemonData();
+  }, []);
+
   return <div>DetailPage</div>;
 };
 
