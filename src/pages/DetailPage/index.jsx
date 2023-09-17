18650220e3e552f5a04b6e7c39252598d2ee5393
seung1 <97tmddnjs@naver.com>
2023-09-17 12:52:28 +0900

    포켓몬 상세데이터 포맷팅

--- src/pages/DetailPage/index.jsx
+++ src/pages/DetailPage/index.jsx
@@ -1,11 +1,14 @@
 import axios from "axios";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const DetailPage = () => {
   const params = useParams();
-  const pokemonId = params.id;
 
+  const [pokemon, setPokemon] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+
+  const pokemonId = params.id;
   const baseUrl = `https://pokeapi.co/api/v2/pokemon/`;
 
   const getNextAndPreviousPokemon = async (id) => {
@@ -23,6 +26,28 @@ const DetailPage = () => {
     };
   };
 
+  const formatPokemonAbilities = (abilities) => {
+    return abilities
+      .filter((_, index) => index <= 1)
+      .map((obj) => obj.ability.name.replaceAll("-", " "));
+  };
+
+  const formatPokemonStats = ([
+    statHP,
+    statATK,
+    statDEP,
+    statSATK,
+    statSDEP,
+    statSPD,
+  ]) => [
+    { name: "Hit Points", baseStat: statHP.base_stat },
+    { name: "Attack", baseStat: statATK.base_stat },
+    { name: "Defense", baseStat: statDEP.base_stat },
+    { name: "Special Attack", baseStat: statSATK.base_stat },
+    { name: "Special Defense", baseStat: statSDEP.base_stat },
+    { name: "Speed", baseStat: statSPD.base_stat },
+  ];
+
   const fetchPokemonData = async () => {
     const url = `${baseUrl}${pokemonId}`;
     try {
@@ -32,19 +57,41 @@ const DetailPage = () => {
         const { name, id, types, weight, height, stats, abilities } =
           pokemonData;
 
+        // 안에 있는 비동기 작업이 모두 완료된 후에 리턴하기 위해 Promise.all() 사용
+        const DamageRelations = await Promise.all(
+          types.map(async (i) => {
+            const type = await axios.get(i.type.url);
+            return type.data.damage_relations;
+          })
+        );
+
         const nextAndPreviousPokemon = await getNextAndPreviousPokemon(id);
 
-        // 진행중
-        console.log(nextAndPreviousPokemon);
+        const formattedPokemonData = {
+          id,
+          name,
+          weight: weight / 10,
+          height: height / 10,
+          previous: nextAndPreviousPokemon.previous,
+          next: nextAndPreviousPokemon.next,
+          abilities: formatPokemonAbilities(abilities),
+          stats: formatPokemonStats(stats),
+          DamageRelations,
+        };
+
+        setPokemon(formattedPokemonData);
       }
     } catch (e) {
       console.error(e);
     }
+    setIsLoading(false);
   };
 
   useEffect(() => {
     fetchPokemonData();
   }, []);
+
+  if (isLoading) return <div>...loading</div>;
 
   return <div>DetailPage</div>;
 };
